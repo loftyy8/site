8f1e6395c4eae34e0913256a20b9cfb1fda7f752
James Lofthouse <loftyy88@googlemail.com>
2017-10-30 09:31:45 +0000

    Added Foot and 3 other files

---
+++ _uploads/head.php
@@ -0,0 +1,12 @@
+<?php echo'
+
+<head>
+	<title>James Lofthouse &bull; Portfolio &bull; Contact</title>
+	<meta charset="utf-8"/>
+    <meta name="Description" CONTENT="Author: J.M.Lofthouse, A website Portfolio">
+    <!--<meta name="google-site-verification" content="+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34="/>-->
+    <meta name="robots" content="index,follow">
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<script src="script.js"></script>
+</head>
+';?>
