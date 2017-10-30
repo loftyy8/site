67fc83b66c4a9ca6396c7c9d86a66ffa24d06484
loftyy8 <loftyy88@googlemail.com>
2017-10-30 09:21:02 +0000

    uploaded files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype HTML>
+<?php include 'head.htm';?>
+<body>
+<section>
+<?php include 'nav.htm';?>
+
+<!-- inc the cont.php -->
+<?php include 'cont.php';?>
+
+<?php include 'foot.htm';?>
+</section>
+</body>
+</html>
